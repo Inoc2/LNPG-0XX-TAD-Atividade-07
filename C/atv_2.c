576ac9a1178c8f09abfee009f99073f16ed69b88
Alvacy Gonçalves <138629360+Inoc2@users.noreply.github.com>
2024-03-27 21:28:53 -0300

    Add files via upload

---
+++ C/atv_2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+typedef struct Xadrez{
+    char tipo_peca;
+    char cor_peca;
+};
+
+char moverPeca(char posicao_peca, char tipo_peca, char cor_peca){
+    return 'a';
+}
+char capturarPeca(char posicao_peca, char tipo_peca, char cor_peca){
+    return 'a';
+}
+int mover_peca(char posicao_peca, char tipo_peca, char cor_peca){
+    return 1;
+}
+char obterInformacoesPeca(char posicao_peca, char tipo_peca, char cor_peca){
+    return 'a';
+}
+
+int main() {
+    return 0;
+}
+
